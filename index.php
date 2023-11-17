2fa686f3e98ec1dca606a2c4148885816df76222
Glenn Welser <glenn.welser@qgiv.com>
2023-11-17 14:34:27 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+class Index
+{
+    private int $id;
+    private string $name;
+    private bool $active;
+
+    public function __construct(int $id, int $name, bool $active = true)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->active = $active;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+        return $this;
+    }
+}
